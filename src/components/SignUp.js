5e3aca140145a9b721350e33fc5105c3ed903539
Beau Elliott <belliott1516@gmail.com>
2023-02-24 11:31:37 -0800

    got sign in and sign up working with supabase

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -20,49 +20,85 @@ export default function SignUp({ setUser }) {
     e.preventDefault();
 
     const user = await signUp(signUpData.email, signUpData.password);
-    await createProfile(signUpData);
-    await createFilter(signUpData);
+    await createProfile(signUpData, user.id);
+    await createFilter(signUpData, user.id);
     setUser(user);
-    console.log(signUpData);
     push('/search');
   }
 
   return (
-    <div className='signUpPage'>
-      <div className='signUp'>
+    <div className="signUpPage">
+      <div className="signUp">
         <h1>Sign Up</h1>
         <form onSubmit={handleSignUp}>
-          <label> Username:
-            <input required type='username' onChange={(e) => setSignUpData({
-              ...signUpData, 
-              username: e.target.value,           
-            })}/>
+          <label>
+            {' '}
+            Username:
+            <input
+              required
+              type="username"
+              onChange={(e) =>
+                setSignUpData({
+                  ...signUpData,
+                  username: e.target.value,
+                })
+              }
+            />
           </label>
-          <label>Email:
-            <input required type='email' onChange={ e => setSignUpData({
-              ...signUpData,
-              email: e.target.value,
-            })}/></label>
-          <label>Password:
-            <input required type='password' onChange={ e => setSignUpData({
-              ...signUpData,
-              password: e.target.value
-            })}/></label>
-          <label> Price Range: 
+          <label>
+            Email:
+            <input
+              required
+              type="email"
+              onChange={(e) =>
+                setSignUpData({
+                  ...signUpData,
+                  email: e.target.value,
+                })
+              }
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              required
+              type="password"
+              onChange={(e) =>
+                setSignUpData({
+                  ...signUpData,
+                  password: e.target.value,
+                })
+              }
+            />
+          </label>
+          <label>
+            {' '}
+            Price Range:
             <CustomSlider setSignUpData={setSignUpData} signUpData={signUpData} />
           </label>
-          <label> Zip Code: 
-            <input required type='number' onChange={(e) => setSignUpData({
-              ...signUpData,
-              zip_code: e.target.value
-            })}/>
+          <label>
+            {' '}
+            Zip Code:
+            <input
+              required
+              type="number"
+              onChange={(e) =>
+                setSignUpData({
+                  ...signUpData,
+                  zip_code: e.target.value,
+                })
+              }
+            />
           </label>
           <button>Sign Up</button>
         </form>
-        <Link to={'/'} className='link'>Sign In</Link>
-        <Link to={'/about'} className='link'>About</Link>
+        <Link to={'/'} className="link">
+          Sign In
+        </Link>
+        <Link to={'/about'} className="link">
+          About
+        </Link>
       </div>
-      
     </div>
   );
 }
--- src/services/supabase-utils.js
+++ src/services/supabase-utils.js
@@ -1,112 +1,90 @@
 import { client } from './client';
 
-export async function getUser(){
-  return await client.auth.session() && await client.auth.session().user;
+export async function getUser() {
+  return (await client.auth.session()) && (await client.auth.session().user);
 }
 
-export async function signIn(email, password){
+export async function signIn(email, password) {
   const { user } = await client.auth.signIn({ email, password });
   return user;
 }
 
-export async function signUp(email, password){
+export async function signUp(email, password) {
   const { user } = await client.auth.signUp({ email, password });
   return user;
 }
 
-export async function logout(){
+export async function logout() {
   await client.auth.signOut();
   return (window.location.href = '../');
 }
 
-export async function getFavoriteHomes(){
-  const { body } = await client
-    .from('saved')
-    .select('*');
+export async function getFavoriteHomes() {
+  const { body } = await client.from('saved').select('*');
 
   return body;
 }
 
-export async function getProfileByID(id){
-  const { body } = await client
-    .from('profiles')
-    .select('*')
-    .match({ user_id: id })
-    .single();
+export async function getProfileByID(id) {
+  const { body } = await client.from('profiles').select('*').match({ user_id: id }).single();
 
   return body;
 }
 
 export async function getFilters() {
   try {
-
-    const { body } = await client
-      .from('filters')
-      .select('*')
-      .single();    
+    const { body } = await client.from('filters').select('*').single();
     return body;
   } catch (e) {
     console.log(e);
   }
 }
 
-export async function createProfile({ username }) {
-  const { body } = await client
-    .from('profiles')
-    .insert({ username });
+export async function createProfile({ username }, user_id) {
+  const { body } = await client.from('profiles').insert({ username, user_id });
 
   return body;
 }
 
-export async function createFilter({ zip_code, low_price, high_price }){
+export async function createFilter({ zip_code, low_price, high_price }, user_id) {
+  const profile = await getProfileByID(user_id);
+  console.log('profile', profile);
   const { body } = await client
     .from('filters')
-    .insert({ zip_code, low_price, high_price });
+    .insert({ zip_code, low_price, high_price, profile_id: profile.id });
   console.log(body, 'filters body');
   return body;
 }
 
-export async function updateFilter({ zip_code, low_price, high_price, id }){
+export async function updateFilter({ zip_code, low_price, high_price, id }) {
   const { body } = await client
     .from('filters')
     .update({ zip_code, low_price, high_price })
     .match({ id });
   return body;
-
 }
-export async function updateProfile({ username, id }){
-  const { body } = await client
-    .from('profiles')
-    .update({ username })
-    .match({ id });
+export async function updateProfile({ username, id }) {
+  const { body } = await client.from('profiles').update({ username }).match({ id });
 
   return body;
 }
 
-export async function createSavedHome(savedHome){
-  const { body } = await client
-    .from('saved')
-    .insert(savedHome);
+export async function createSavedHome(savedHome) {
+  const { body } = await client.from('saved').insert(savedHome);
 
   return body;
 }
 
-export async function deleteSavedHome(property_id){
-  const { body } = await client
-    .from('saved')
-    .delete()
-    .match({ property_id });
+export async function deleteSavedHome(property_id) {
+  const { body } = await client.from('saved').delete().match({ property_id });
 
   return body;
 }
 
-export async function uploadAvatar(imageFile){
-  const { body } = await client
-    .storage
-    .from('avatar')
-    .upload(imageFile, {
-      cacheControl: '3600',
-      upsert: true
-    });
+export async function uploadAvatar(imageFile) {
+  const { body } = await client.storage.from('avatar').upload(imageFile, {
+    cacheControl: '3600',
+    upsert: true,
+  });
   return body;
 }
